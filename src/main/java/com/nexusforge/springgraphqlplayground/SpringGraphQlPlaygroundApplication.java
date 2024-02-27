6cac9396eadf3fe3ab1132f1d5a9733c1844ac16
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-02-27 14:25:31 +0100

    01 -  Simple Graphql Server

---
+++ src/main/java/com/nexusforge/springgraphqlplayground/SpringGraphQlPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.nexusforge.springgraphqlplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphQlPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGraphQlPlaygroundApplication.class, args);
+    }
+
+}
