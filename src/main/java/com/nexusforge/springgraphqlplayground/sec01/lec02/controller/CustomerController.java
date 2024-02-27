1d074bbfaf583cf3704cce031a6c049b9f819712
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-02-27 15:36:30 +0100

    03 - Lec02 Implementation of Custom Types

---
+++ src/main/java/com/nexusforge/springgraphqlplayground/sec01/lec02/controller/CustomerController.java
@@ -0,0 +1,32 @@
+package com.nexusforge.springgraphqlplayground.sec01.lec02.controller;
+
+import com.nexusforge.springgraphqlplayground.sec01.lec02.entity.Customer;
+import com.nexusforge.springgraphqlplayground.sec01.lec02.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Controller
+public class CustomerController {
+
+    @Autowired
+    private CustomerService service;
+
+    @QueryMapping("customers")
+    public Flux<Customer> customers() {
+        return this.service.allCustomer();
+    }
+
+    @QueryMapping("customerById")
+    public Mono<Customer> customerById(@Argument("id") Integer id) {
+        return this.service.customerById(id);
+    }
+
+    @QueryMapping("customersNameContains")
+    public Flux<Customer> customersNameContains(@Argument("name") String name) {
+        return this.service.nameContains(name);
+    }
+}
---
+++ src/main/java/com/nexusforge/springgraphqlplayground/sec01/lec02/service/CustomerService.java
@@ -0,0 +1,30 @@
+package com.nexusforge.springgraphqlplayground.sec01.lec02.service;
+
+import com.nexusforge.springgraphqlplayground.sec01.lec02.entity.Customer;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class CustomerService {
+    private final Flux<Customer> flux = Flux.just(
+            Customer.create(1, "sam", 20, "atlanta"),
+            Customer.create(2, "Jake", 230, "las vegas"),
+            Customer.create(3, "Mike", 60, "miami"),
+            Customer.create(4, "John", 13, "Georgia"),
+            Customer.create(5, "Lucy", 2, "houston")
+    );
+
+    public Flux<Customer> allCustomer() {
+        return flux;
+    }
+
+    public Mono<Customer> customerById(Integer id) {
+        return flux.filter(c -> c.getId().equals(id))
+                .next();
+    }
+
+    public Flux<Customer> nameContains(String name) {
+        return flux.filter(c -> c.getName().contains(name));
+    }
+}
