c81012cf6e5226b1bd4d033e37fb9e4d0517b4a4
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-03-06 01:39:06 +0100

    04 - Lec12 -  implementation of custom problems

--- src/main/java/com/nexusforge/springgraphqlplayground/SpringGraphQlPlaygroundApplication.java
+++ src/main/java/com/nexusforge/springgraphqlplayground/SpringGraphQlPlaygroundApplication.java
@@ -3,7 +3,7 @@ package com.nexusforge.springgraphqlplayground;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.nexusforge.springgraphqlplayground.sec01.lec05")
+@SpringBootApplication(scanBasePackages = "com.nexusforge.springgraphqlplayground.lec06")
 public class SpringGraphQlPlaygroundApplication {
 
     public static void main(String[] args) {
