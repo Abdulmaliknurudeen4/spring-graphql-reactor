d808c128ee08c5d14bfd47093462cba798ecdfbe
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-03-10 15:00:22 +0100

    04 - Lec15 Working with Scalar Types and Exposing Object Types.

--- src/main/java/com/nexusforge/springgraphqlplayground/lec09/controller/ScalarController.java
+++ src/main/java/com/nexusforge/springgraphqlplayground/lec09/controller/ScalarController.java
@@ -1,9 +1,11 @@
-package com.nexusforge.springgraphqlplayground.lec08;
+package com.nexusforge.springgraphqlplayground.lec09.controller;
 
 import com.nexusforge.springgraphqlplayground.lec09.dto.AllTypes;
 import com.nexusforge.springgraphqlplayground.lec09.dto.Car;
+import com.nexusforge.springgraphqlplayground.lec09.dto.Product;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
@@ -11,6 +13,7 @@ import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
+import java.util.Map;
 import java.util.UUID;
 
 @Controller
@@ -36,5 +39,19 @@ public class ScalarController {
     public Mono<AllTypes> get() {
         System.out.println(allTypes.getId());
         return Mono.just(allTypes);
+    }
+
+    @QueryMapping
+    public Flux<Product> products(){
+        return Flux.just(
+                Product.create("banana", Map.of(
+                        "expiry date", "01/01/2025",
+                        "color", "yellow"
+                )),
+                Product.create("Mac", Map.of(
+                        "cpu", "8",
+                        "RAM", "32g"
+                ))
+        );
     }
 }
